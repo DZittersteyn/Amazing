b070f2a950026b7ef9b7aecc5b50013ea6a529d7
Dirk Zittersteyn <Zittersteyn@gmail.com>
2012-01-11 18:06:44 +0100

    result of Django tutorial 1 through 3

---
+++ amazing/polls/admin.py
@@ -0,0 +1,18 @@
+from polls.models import Poll, Choice
+from django.contrib import admin
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+    list_display = ('question', 'pub_date', 'was_published_today')
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'] , 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
